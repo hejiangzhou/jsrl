f49f730da4bd98440b685ed19f9283e7dfd35074
Jiangzhou He <hejiangzhou@gmail.com>
2013-04-11 14:58:38 +0800

    Better fix of previous issue.

--- out/release/jsutils.js
+++ out/release/jsutils.js
@@ -933,7 +933,7 @@ h.push(f);
 _exec : function () {
 var self = this;
 var q = this._queue;
-var nextTime = (new Date()).getTime() + Q.SCHEDULER_INTERVAL / 2;
+var nextTime = (new Date()).getTime() + Q.SCHEDULER_INTERVAL * 3 / 2;
 var i = 0;
 self._current = 0;
 var e = function () {
